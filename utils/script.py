ccacd15eeb6b48c46cc0f43762958244f90b9cbd
Anuu <110338926+Manishgupta64047@users.noreply.github.com>
2023-04-21 17:22:28 +0530

    Update script.py

--- utils/script.py
+++ utils/script.py
@@ -1,29 +1,29 @@
 class script(object):
-    START = """**Hello {} 🤟**
+    START = """**Hello Dear {}🤠🍿**
    
-I am **Find Post Bot**.I am best Channel Link Search Bot! 
+I am **Channel Post Filter Bot**.I am OP Channel Post Filterer & Provider Bot! 
 I Will filter your channel posts automatically and send it in your group chat when Someone search it."""
 
     HELP = """To Use me In A Group
 
-- Add me in your group & channel with all permissions. 
-- Send /verify in group & wait for It To Accept Or Directly Contact To Owner After Request @GreyMatter_Bots.
+- Add me in your group & channel with all permissions,except remains anonymous one. 
+- Send /verify in group & wait for It To Accept Or Directly Contact To Owner After Request @Aay700.
 - After verification send /connect YourChannelID
 - Example : /connect -100xxxxxxxxxx
-- Done ✅. <b><I>Enjoy 💜❤</I></b> )
+- Done ✅. <b><I>Just Make Sure ur Channel Is Public 🥳 Also u can try our alternate bot if u want 🔗 to send from DB channel!</I></b> )
 
 Remove a Channel with - /disconnect-100xxxxxxxxxxx
-this will help you to remove a indexed channel from your group.
+this will help you to remove an indexed channel from your group.
 
-Get connected channels list with - /connections"""
+Get connected channels list with /connections i.e u can connect ur multiple channels simultaneously 🤑-"""
 
-    ABOUT = """Developed By @CyniteBackup
+    ABOUT = """Developed By @mdisk_bots
 
-✯ Mʏ Nᴀᴍᴇ:  {}
-✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/CyniteOfficial'>Harman</a>
-✯ Lᴀɴɢᴜᴀɢᴇ: <a href='https://www.python.org'>Pʏᴛʜᴏɴ 3</a>
+✯ Alternate Bot: <a href='https://t.me/Mdisk_Link_Downloader_Bot'>Need a D.B Channel</a>
+✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/z_harbour'>Channel</a>
+✯ Lᴀɴɢᴜᴀɢᴇ: <a href='https://www.python.org'>Pʏᴛʜᴏɴ v3</a>
 ✯ DᴀᴛᴀBᴀsᴇ: <a href='https://cloud.mongodb.com/'>MᴏɴɢᴏDB</a>
-✯ Bᴏᴛ Sᴇʀᴠᴇʀ: : <a href='https://heroku.com'>Heroku</a>"""
+✯ Bᴏᴛ Sᴇʀᴠᴇʀ: : <a href='https://heroku.com'>Paid Server</a>"""
 
     STATS = """My Status 💫
 
