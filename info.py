a1e13025664171a2114213b57b3da709003e5adc
Anuu <110338926+Manishgupta64047@users.noreply.github.com>
2023-04-21 17:41:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,7 +2,7 @@ API_ID       = 12267130
 API_HASH     = "6ddb28a13ffc2f15dc16f06be6ca3c1f"
 BOT_TOKEN    = "6019847133:AAHFcXabIXDJvERwbWfsBtOJf_X2XfHB6Rg"
 SESSION      = "BQC7LnoADQFAUIcUFNwvSXehcRkdYxN-Bl_jpZC5nHJCu45EbgjwbnPJAJZAPscHVH7QC2vGXFEuEpxk-TWyXvrAydQNyC-opme9EFt-uZd8n_rUjQ1oZeBT9-KPHKdQOVJi5e8GfF9Z3pjLM84klRTN5ElDBTIsp8V3L_LE5ulFBSnaEOjyorS-r-6DD0CWIHxhUpmReaXbO6_K5OxxnvjWTi8HTql5GkkLDIHqnl-x5TCDc-xfmRmGm1YA1jFGiIapSLgksfOiSBbUIKPYoCPhWleo8T8EySyKUqoMtan0A-WUYufDiS5T5krgc9DyJ_tsGV1WhayD2UYbgPS5hdKC0G3sTgAAAAE5dsIAAA"
-DATABASE_URI = "mongodb+srv://herokudeploye:herokudeploye@cluster0.m9r3mk8.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://channelbot:channelbot@cluster0.anurlrs.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001884373226
 ADMIN        = 1350642629
 CHANNEL      = "@z_harbour"
